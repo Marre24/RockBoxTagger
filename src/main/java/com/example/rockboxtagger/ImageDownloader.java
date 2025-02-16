f3ca75b6b481ec54e45fcc750ac781932b16bb80
maxi <maxi.ellnestam@gmail.com>
2025-02-16 18:21:40 +0100

    Add path to ImageDownloader

--- src/main/java/com/example/rockboxtagger/ImageDownloader.java
+++ src/main/java/com/example/rockboxtagger/ImageDownloader.java
@@ -4,12 +4,14 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.Collections;
 
 public abstract class ImageDownloader {
+    final static String COVER_ART_PATH = "./CoverArt";
     public static void downloadImage(String imageUrl, String fileName) throws IOException {
         URL url = new URL(imageUrl);
         try (InputStream inputStream = url.openStream();
-             FileOutputStream outputStream = new FileOutputStream(fileName)) {
+             FileOutputStream outputStream = new FileOutputStream(COVER_ART_PATH + fileName)) {
 
             byte[] buffer = new byte[4096];
             int bytesRead;
