7f886aabbe1224ef76b075d96ce20d97daaf9565
maxi <maxi.ellnestam@gmail.com>
2025-02-17 21:15:13 +0100

    Refactored to LinkedList

--- src/main/java/com/example/rockboxtagger/FolderReader.java
+++ src/main/java/com/example/rockboxtagger/FolderReader.java
@@ -2,11 +2,12 @@ package com.example.rockboxtagger;
 
 import java.io.File;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 public class FolderReader {
 
-    public static List<String> getAlbumNames(String path){
+    public static LinkedList<String> getAlbumNames(String path){
 
         File file = new File(path + "\\Albums");
 
@@ -19,7 +20,7 @@ public class FolderReader {
             System.err.println("Could not find any sub-folders to directory: " + file.getAbsolutePath());
             return null;
         }
-        return Arrays.asList(directories);
+        return new LinkedList<>(Arrays.asList(directories));
     }
 
 }
