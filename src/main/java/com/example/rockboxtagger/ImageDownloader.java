121a468e7ffef7405c724f2d3921c98c9aafa90e
maxi <maxi.ellnestam@gmail.com>
2025-02-17 21:14:01 +0100

    Add removeFile

--- src/main/java/com/example/rockboxtagger/ImageDownloader.java
+++ src/main/java/com/example/rockboxtagger/ImageDownloader.java
@@ -1,26 +1,51 @@
 package com.example.rockboxtagger;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Collections;
 
 public abstract class ImageDownloader {
-    final static String COVER_ART_PATH = "./CoverArt";
-    public static void downloadImage(String imageUrl, String fileName) throws IOException {
-        URL url = new URL(imageUrl);
+    private final static String COVER_ART_PATH = ".\\CoverArt\\";
+    private final static String ENDING = ".jpg";
+    public static void downloadImage(String imageUrl, String fileName){
+        URL url = null;
+        try {
+            url = new URL(imageUrl);
+        } catch (MalformedURLException e) {
+            System.err.println("Could not open " + imageUrl + ": " + e.getMessage());
+            return;
+        }
         try (InputStream inputStream = url.openStream();
-             FileOutputStream outputStream = new FileOutputStream(COVER_ART_PATH + fileName)) {
+             FileOutputStream outputStream = new FileOutputStream(buildPath(fileName))) {
 
             byte[] buffer = new byte[4096];
             int bytesRead;
             while ((bytesRead = inputStream.read(buffer)) != -1) {
                 outputStream.write(buffer, 0, bytesRead);
             }
-            System.out.println("Downloaded image: " + fileName);
+            System.out.println("Downloaded image: " + buildPath(fileName));
         } catch (IOException e) {
             System.err.println("Failed to download image: " + e.getMessage());
         }
+    }
+
+    public static boolean removeFile(String filename){
+
+        File fileToDelete = new File(buildPath(filename));
+
+        if (!fileToDelete.isFile()){
+            System.err.println(fileToDelete + " is not a file");
+            return false;
+        }
+
+        return fileToDelete.delete();
+    }
+
+    public static String buildPath(String s){
+        return COVER_ART_PATH + s + ENDING;
     }
 }
