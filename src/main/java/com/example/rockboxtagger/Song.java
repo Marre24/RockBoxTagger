d1fc99b963fbd36d342f19798f106605905a4551
maxi <maxi.ellnestam@gmail.com>
2025-02-18 16:26:44 +0100

    Add writeToTag method

--- src/main/java/com/example/rockboxtagger/Song.java
+++ src/main/java/com/example/rockboxtagger/Song.java
@@ -1,8 +1,13 @@
 package com.example.rockboxtagger;
 
-import java.util.List;
+import org.jaudiotagger.tag.FieldDataInvalidException;
+import org.jaudiotagger.tag.FieldKey;
+import org.jaudiotagger.tag.Tag;
 
-public record Song(String title, String artist, String duration, int trackNumber, List<String> features) {
+import java.util.List;
+import java.util.Objects;
+
+public record Song(String title, String duration, int trackNumber, List<String> features) {
     @Override
     public String toString() {
         if (!features().isEmpty()){
@@ -17,5 +22,28 @@ public record Song(String title, String artist, String duration, int trackNumber
         }
 
         return trackNumber + ". " + title + " " + duration;
+    }
+
+    public void writeToTag(Tag tag, Album album) throws FieldDataInvalidException {
+        tag.setField(FieldKey.ALBUM_ARTIST, album.artist());
+        tag.setField(FieldKey.ALBUM, album.title());
+        tag.setField(FieldKey.TITLE, title);
+        tag.setField(FieldKey.TRACK, Integer.toString(trackNumber));
+        tag.setField(FieldKey.YEAR, Integer.toString(album.year()));
+        tag.setField(FieldKey.GENRE, album.genre());
+        tag.setField(FieldKey.COVER_ART, album.imageUrl());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Song song = (Song) o;
+        return trackNumber == song.trackNumber && Objects.equals(title, song.title) && Objects.equals(features, song.features);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, trackNumber, features);
     }
 }
