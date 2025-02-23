4d7d9e0a11c01cb106265953561a4a7611c3f9f1
maxi <maxi.ellnestam@gmail.com>
2025-02-23 20:49:09 +0100

    Refactored feat title

--- src/main/java/com/example/rockboxtagger/Song.java
+++ src/main/java/com/example/rockboxtagger/Song.java
@@ -3,36 +3,44 @@ package com.example.rockboxtagger;
 import org.jaudiotagger.tag.FieldDataInvalidException;
 import org.jaudiotagger.tag.FieldKey;
 import org.jaudiotagger.tag.Tag;
+import org.jaudiotagger.tag.datatype.Artwork;
+import org.jaudiotagger.tag.reference.PictureTypes;
 
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.Objects;
 
 public record Song(String title, String duration, int trackNumber, List<String> features) {
+    private String getFeatureTitle(){
+        if (features().isEmpty())
+            return title;
+
+        StringBuilder featureString = new StringBuilder("(feat. ");
+
+        for (var f : features())
+            featureString.append(f);
+
+        featureString.append(")");
+
+        return title + featureString;
+    }
     @Override
     public String toString() {
-        if (!features().isEmpty()){
-            StringBuilder featureString = new StringBuilder("(feat. ");
-
-            for (var f : features())
-                featureString.append(f);
-
-            featureString.append(")");
-
-            return trackNumber + ". " + title + featureString + " " + duration;
-        }
-
-        return trackNumber + ". " + title + " " + duration;
+        return trackNumber + ". " + getFeatureTitle() + " " + duration;
     }
 
-    public void writeToTag(Tag tag, Album album) throws FieldDataInvalidException {
+    public void writeToTag(Tag tag, Album album) throws FieldDataInvalidException, IOException {
         tag.setField(FieldKey.ALBUM_ARTIST, album.artist());
+        tag.setField(FieldKey.ARTIST, album.artist());
         tag.setField(FieldKey.ALBUM, album.title());
-        tag.setField(FieldKey.TITLE, title);
+        tag.setField(FieldKey.TITLE, getFeatureTitle());
         tag.setField(FieldKey.TRACK, Integer.toString(trackNumber));
         tag.setField(FieldKey.YEAR, Integer.toString(album.year()));
         tag.setField(FieldKey.GENRE, album.genre());
-        //tag.setField(FieldKey.COVER_ART, PathBuilder.buildCommitPath(Long.toString(album.masterID())));
     }
 
     @Override
